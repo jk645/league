455784a7d1d135d52e7370fcfbec8b8617faf8df
Josiah Kruse <sik645@gmail.com>
2025-01-09 13:05:12 -0800

    Build out player details page

--- Pages/Players/Player.cshtml.cs
+++ Pages/Players/Player.cshtml.cs
@@ -1,12 +1,33 @@
+using League.Data;
+using League.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace League.Pages.Players
 {
     public class PlayerModel : PageModel
     {
-        public void OnGet()
+        private readonly LeagueContext _context;
+
+        public PlayerModel(LeagueContext context)
         {
+            _context = context;
+        }
+
+        public Player Player { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(string playerid)
+        {
+            Player = await _context.Players
+                .FirstOrDefaultAsync(p => p.PlayerId == playerid);
+
+            if (Player == null) {
+                return NotFound();
+            }
+
+            return Page();
         }
     }
 }
