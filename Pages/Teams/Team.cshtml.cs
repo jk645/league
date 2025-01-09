b24d8961b1eb1bff4f6c56ccbf20e514c0c63bd8
Josiah Kruse <sik645@gmail.com>
2025-01-09 12:38:16 -0800

    Set up fetching team details

--- Pages/Teams/Team.cshtml.cs
+++ Pages/Teams/Team.cshtml.cs
@@ -1,12 +1,34 @@
+using League.Data;
+using League.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace League.Pages.Teams
 {
     public class TeamModel : PageModel
     {
-        public void OnGet()
+        private readonly LeagueContext _context;
+
+        public TeamModel(LeagueContext context)
         {
+            _context = context;
+        }
+
+        public Team Team { get; set; }
+
+        public async Task<ActionResult> OnGetAsync(string teamid)
+        {
+            Team = await _context.Teams
+                .FirstOrDefaultAsync(t => t.TeamId == teamid);
+
+            if (Team == null)
+            {
+                return NotFound();
+            }
+            return Page();
         }
     }
 }
